5003ba03b3471d90b391d3334ace82590d89e233
otani <hamish07020702@gmail.com>
2022-11-04 11:12:50 +0900

    1回目のコミット

---
+++ js/task-javascript.js
@@ -0,0 +1,8 @@
+window.addEventListener(“load”, function() {
+  const getNode = document.getElementById(“get-node”);
+  console.log(getNode);
+  getNode.append(“追加しました”);
+  getNode.innerHTML = `<h1>JavaScriptの勉強</h1>`;
+  })
+
+  
